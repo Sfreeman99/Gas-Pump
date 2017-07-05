c16b62396194a894eea89bfd2f5f0306e4b0d9ad
Shedlia.Freeman <shedliaf@basecampcodingacademy.org>
2017-07-05 10:59:50 -0500

    Both the shell and core are operational plus a few changes in the if statements and parameters

--- gas_core.py
+++ gas_core.py
@@ -1,33 +1,33 @@
+import gas_shell
 
-
-def do_prepay():
+def do_prepay(prepay, gas_type):
     ''' (float, string) -> None
     Calculates how much gas you would get when you
     input a number
 
     '''
-
-    if gas =='premium':
-         print('Your total gallons would be:  ',round(float(prepay) / 2.49),'gal', sep= ' ')
-    elif gas == 'regular':
-        print('Your total gallons would be: $ ', round(float(prepay)/ 2.07),'gal', sep=' ')
-    elif gas =='mid grade':
-        print('Your total gallons would be: ', round(float(prepay)/ 2.10),'gal', sep=' ')
+    
+    if gas_type =='premium':
+         print('Your total gallons would be:  ',round(float(prepay) / 2.49, 2),'gal', sep= ' ')
+    elif gas_type == 'regular':
+        print('Your total gallons would be: $ ', round(float(prepay)/ 2.07, 2),'gal', sep=' ')
+    elif gas_type =='mid grade':
+        print('Your total gallons would be: ', round(float(prepay)/ 2.10, 2),'gal', sep=' ')
 
     else:
         print('Ummmmmm Me no Compute')
      
-def do_gas_after_pumping():
-    """(gal) -> price
+def do_gas_after_pumping(gas_price,gas_type):
+    """(str) -> price of gas
     Calculates after pumping how much money you would
     have to pay after pumping your gas
     """
     if gas_type == 'premium':
-        print('Your total is: $',round(float(gas_price)  * 2.49), sep='')
+        print('Your total is: $',round(float(gas_price)  * 2.49, 2), sep='')
     elif gas_type == 'regular' :
-        print('Your total is: $',round(float(gas_price)  * 2.07), sep='')
+        print('Your total is: $',round(float(gas_price)  * 2.07, 2), sep='')
     elif gas_type == 'mid grade' :
-        print('Your total is: $', round(float(gas_price)  * 2.10), sep='')
+        print('Your total is: $', round(float(gas_price)  * 2.10, 2), sep='')
 
     else:
         print('Ummmmm Me no Compute')
--- gas_shell.py
+++ gas_shell.py
@@ -1,11 +1,14 @@
+import time
+import gas_core
+
 input("Welcome to Shedlia's gas Station!! How are you doing? \n")
 paying_type = input('would you like to prepay?\n\tYes\n\tNo\n').upper()
 if paying_type== 'Yes'.upper():
     prepay=input("How much money would you like to put in?\n$: ")
-    gas = input("Which gas would you like to choose?: \n\tpremium = 2.49\n\tregular = 2.07\n\tmid grade = 2.10\n")
+    gas_type = input("Which gas would you like to choose?: \n\tpremium = 2.49\n\tregular = 2.07\n\tmid grade = 2.10\n")
     print('pumping...')
     time.sleep(5.5)
-    do_prepay()
+    gas_core.do_prepay(prepay, gas_type)
 
 elif paying_type == 'No'.upper():
     print('pumping...')
@@ -13,7 +16,7 @@ elif paying_type == 'No'.upper():
     gas_price=input("How many gallons did you put in?\n")
     gas_type=input('Which gas did you choose?\n\tpremium \n\tregular \n\tmid grade\n')
     
-    do_gas_after_pumping()
+    gas_core.do_gas_after_pumping(gas_price,gas_type)
 
 else :
     print(".......")
