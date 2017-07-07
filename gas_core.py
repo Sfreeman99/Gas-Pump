40c68da159d076a439f2505c97a00c11b83fde80
Shedlia.Freeman <shedliaf@basecampcodingacademy.org>
2017-07-07 10:51:13 -0500

    for the if statement in the gas_shell the first if statement is fully working and a new function was made in the core

--- gas_core.py
+++ gas_core.py
@@ -1,59 +1,67 @@
+# def do_prepay(prepay, gas_type):
+#     ''' (float, string) -> None
+#     Calculates how much gas you would get when you
+#     input a number
 
-
-def do_prepay(prepay, gas_type):
-    ''' (float, string) -> None
-    Calculates how much gas you would get when you
-    input a number
-
-    >>> do_prepay(2.49, 'premium')
-    '1.00 gal'
-    '''
+#     >>> do_prepay(2.49, 'premium')
+#     '1.00 gal'
+#     '''
     
-    # if gas_type =='premium':
-    #      return 'Your total gallons would be: {:.2f} gal'.format(float(prepay) / 2.49, 2) 
-    # elif gas_type == 'regular':
-    #     return 'Your total gallons would be: {:.2f} gal'.format(float(prepay)/ 2.07, 2)
-    # elif gas_type =='mid grade':
-    #     return 'Your total gallons would be: {:.2f} gal'.format(float(prepay)/ 2.10, 2)
-
-    # else:
-    #     return 'Ummmmmm Me no Compute'
-    for i in tank:
-        if gas_type == i[0]:
-            return '{:.2f} gal'.format(float(prepay) / i[1])
+#     for item in tank:
+#         if gas_type == item[0]:
+#             return '{:.2f} gal'.format(float(prepay) / item[1])
      
-def do_gas_after_pumping(gallons,gas_type):
-    """(float, str) -> price of gas
-    Calculates after pumping how much money you would
-    have to pay after pumping your gas
+# def do_gas_after_pumping(gallons,gas_type):
+#     """(float, str) -> price of gas
+#     Calculates after pumping how much money you would
+#     have to pay after pumping your gas
 
-    >>> do_gas_after_pumping(1.00, 'premium')
-    '$2.49'
-    >>> do_gas_after_pumping(1.00, 'regular')
-    '$2.07'
-    >>> do_gas_after_pumping(1.00, 'mid grade')
-    '$2.10'
-    """
-    # if gas_type == 'premium':
-    #     return 'Your total is: ${:.2f}'.format(float(gallons)  * 2.49, 2)
-    # elif gas_type == 'regular' :
-    #     return 'Your total is: ${:.2f}'.format(float(gallons)  * 2.07, 2)
-    # elif gas_type == 'mid grade' :
-    #     return 'Your total is: ${:.2f}'.format(float(gallons)  * 2.10, 2)
-    # else:
-    #     return 'Ummmmm Me no Compute'
-    for i in tank:
-        if gas_type == i[0]:
-            return '${:.2f}'.format(float(gallons) * i[1])
+#     >>> do_gas_after_pumping(1.00, 'premium')
+#     '$2.49'
+#     >>> do_gas_after_pumping(1.00, 'regular')
+#     '$2.07'
+#     >>> do_gas_after_pumping(1.00, 'mid grade')
+#     '$2.10'
+#     """
+    
+#     for item in tank:
+#         if gas_type == item[0]:
+#             return '${:.2f}'.format(float(gallons) * item[1])
 
-
-
-
-
-tank = [
-    ['premium', 2.49],
-    ['regular', 2.07],
-    ['mid grade', 2.10]
-]
+def gas_pump(gas_type, money):
+    ''' '''
+    open_inventory()
+    for item in open_inventory():
+        if gas_type.lower().strip() == item[0].lower().strip():
+            gallons = float(money) * float(item[1])
+            return gallons
+            
 
 
+
+def open_inventory():
+    with open('tank.txt', 'r') as tank:
+        tank.readline()
+        str_inventory = tank.readlines()
+
+    inventory = []
+    for item in str_inventory:
+        sublist = item.split(', ')
+        inventory.append([sublist[0], float(sublist[1].strip()), float(sublist[2].strip())])
+
+    return inventory
+
+  
+def update_inventory(inventory, gas_type, gallons):
+    message = 'name, price, quantity\n'
+    for item in inventory:
+        if item[0] == gas_type:
+            item[2] = float(item[2]) - float(gallons)
+        message += ('{0}, {1:.2f}, {2:.2f}\n'.format(item[0],item[1],item[2]))
+    with open('tank.txt','w') as tank:
+        tank.write(message)
+    
+
+
+
+tank = open_inventory()
--- gas_shell.py
+++ gas_shell.py
@@ -2,17 +2,18 @@ import time
 import gas_core
 
 input("Welcome to Shedlia's gas Station!! How are you doing? \n")
+inventory = gas_core.open_inventory()
 paying_type = input('would you like to prepay?\n\tYes\n\tNo\n').upper()
 if paying_type== 'Yes'.upper():
-    prepay=input("How much money would you like to put in?\n$: ")
+    money=input("How much money would you like to put in?\n$: ")
     gas_type = input("Which gas would you like to choose?: \n\tpremium = 2.49\n\tregular = 2.07\n\tmid grade = 2.10\n")
     print('pumping...')
     time.sleep(2)
+    gallons = gas_core.gas_pump(gas_type, money)
     with open('log.txt','a') as history:
 
-        history.write('\n{0}, ${1} , {2}'.format(gas_type,prepay,gas_core.do_prepay(prepay, gas_type)))
-    print(gas_core.do_prepay(prepay,gas_type))
-
+        history.write('\n{0}, ${1} , {2}'.format(gas_type,money,gas_core.gas_pump(gas_type,money)))
+    print('With $',money,' you get ',gas_core.gas_pump(gas_type,money),' gal.')
 elif paying_type == 'No'.upper():
     print('pumping...')
     time.sleep(2)   
@@ -27,3 +28,10 @@ else :
     print(".......")
     time.sleep(2)
     print("Well you can hit the door!!!") 
+
+gas_core.update_inventory(inventory, gas_type, gallons)
+
+            
+    
+    
+
